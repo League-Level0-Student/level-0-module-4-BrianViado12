2c97ec7e896debd205fe2f80f7e3f015c50b4c51
Daddy <Daddy@vivs-macpro-13>
2020-03-26 15:56:51 -0700

    did it

--- src/_04_test_scores/Score.java
+++ src/_04_test_scores/Score.java
@@ -5,12 +5,16 @@ import javax.swing.JOptionPane;
 public class Score {
 	public static void main(String[] args) {
 		String scores = JOptionPane.showInputDialog("What did you get on the test?");
-		double C = Integer.parseInt(scores)* 75.5;
-		double A = Integer.parseInt(scores)* 91.5;
-		double D = Integer.parseInt(scores)* 64.5;
-		double B = Integer.parseInt(scores)* 83.5;
-		if (scores==("C")) {
-			
+		double score  = Double.parseDouble(scores);
+		
+		if (score>=91.5) {
+			JOptionPane.showMessageDialog(null, "Wow you must've studied really hard.");
+		}else if (score>=83.5) {
+			JOptionPane.showMessageDialog(null, "Great, but you could do better.");
+		}else if (score>=72.5) {
+			JOptionPane.showMessageDialog(null, "Better luck next time.");
+		}else if (score>=61.5) {
+			JOptionPane.showMessageDialog(null, "You should study harder.");
 		}
 	}
 }
--- src/_05_sleepy_head/SleepyHead.java
+++ src/_05_sleepy_head/SleepyHead.java
@@ -18,9 +18,25 @@ public class SleepyHead {
          * Ask the user for these values using a confirm dialog like the one below
          * JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
          */
-    
-
-
+        	int answer = JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
+        	if (answer==(0)) {
+				isWeekday = true;
+			}else {
+				isWeekday= false;
+			}
+        	int vacation = JOptionPane.showConfirmDialog(null, "Is it a vacation?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
+			if (vacation==(0)) {
+				isVacation = true;
+			}else {
+				isVacation = false;
+			}
+			if (isVacation==true || isWeekday==false) {
+				System.out.println("Sleep in");
+			} else if (isWeekday==true) {
+				System.out.println("Get up lazybones!");
+			} else if (isVacation== true  && isWeekday== true ) {
+				System.out.println("Sleep in");
+			}
         /*
          * Print “sleep in�? if it is a vacation or a weekend. If it’s a weekday,
          * print “get up lazybones!�? If it is a weekday, and we are on vacation,
